1c8c8811e28419e6a60de0e31816b090553a9b20
hamsame <hamsaaelmi@gmail.com>
2023-02-20 15:04:22 +0000

    added gui to manipulate mesh from ui

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import * as THREE from "three"
 import "./style.css"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import GUI from "lil-gui"
 const loader = new GLTFLoader()
 import model from "./soldier.glb"
 
 const scene = new THREE.Scene()
+const gui = new GUI()
 
 const size = {
   width: 800,
@@ -38,8 +40,6 @@ controls.enableDamping = true
 
 var ob
 const animate = () => {
-  const elapsed = clock.getElapsedTime()
-  ob.rotation.y = elapsed
   controls.update()
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
@@ -48,6 +48,11 @@ const animate = () => {
 loader.load(model, (glb) => {
   ob = glb.scene
   scene.add(ob)
+  ob.rotation.y = 3.15
+  gui.add(ob.position, "x", 0, 3).name("horizontal position")
+  gui.add(ob.position, "y", -3, 3).name("vertical position")
+  gui.add(ob.position, "z", -30, 3).name("closeness")
+  gui.add(ob.rotation, "y", -3.15, 9.45).name("rotation")
   camera.position.set(0, 0, 10)
   animate()
 })
